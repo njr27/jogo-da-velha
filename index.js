b95073a03959b1a192ff2fb4dcc58b4446427257
njr27 <nivaldojunior00@gmail.com>
2022-05-15 18:27:38 -0300

    Refatoração do código proposta na aula 18

--- index.js
+++ index.js
@@ -1,103 +1,93 @@
-const $boardList = document.querySelectorAll(".square");
+const $boardList = document.querySelectorAll(".square")
+const $score1 = document.querySelector(".score-p1")
+const $score2 = document.querySelector(".score-p2")
 
-let currentMove = "X";
+let currentMove = "X"
+let scorePlayer1 = 0
+let scorePlayer2 = 0
+
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 5],
+  [0, 4, 8],
+  [2, 4, 6],
+]
 
 function toggleMove() {
-  if (currentMove == "X") {
-    currentMove = "O";
-  } else {
-    currentMove = "X";
-  }
+  currentMove = currentMove == "X" ? "O" : "X"
 }
 
 function verifyWinner() {
-  if (
-    $boardList[0].innerHTML != "" &&
-    $boardList[0].innerHTML == $boardList[1].innerHTML &&
-    $boardList[0].innerHTML == $boardList[2].innerHTML
-  ) {
-    return currentMove;
-  } else if (
-    $boardList[3].innerHTML != "" &&
-    $boardList[3].innerHTML == $boardList[4].innerHTML &&
-    $boardList[3].innerHTML == $boardList[5].innerHTML
-  ) {
-    return currentMove;
-  } else if (
-    $boardList[6].innerHTML != "" &&
-    $boardList[6].innerHTML == $boardList[7].innerHTML &&
-    $boardList[6].innerHTML == $boardList[8].innerHTML
-  ) {
-    return currentMove;
-  } else if (
-    $boardList[0].innerHTML != "" &&
-    $boardList[0].innerHTML == $boardList[3].innerHTML &&
-    $boardList[0].innerHTML &&
-    $boardList[6].innerHTML
-  ) {
-    return currentMove;
-  } else if (
-    $boardList[1].innerHTML != "" &&
-    $boardList[1].innerHTML == $boardList[4].innerHTML &&
-    $boardList[1].innerHTML == $boardList[7].innerHTML
-  ) {
-    return currentMove;
-  } else if (
-    $boardList[2].innerHTML != "" &&
-    $boardList[2].innerHTML == $boardList[5].innerHTML &&
-    $boardList[2].innerHTML == $boardList[8].innerHTML
-  ) {
-    return currentMove;
-  } else if (
-    $boardList[0].innerHTML != "" &&
-    $boardList[0].innerHTML == $boardList[4].innerHTML &&
-    $boardList[0].innerHTML == $boardList[8].innerHTML
-  ) {
-    return currentMove;
-  } else if (
-    $boardList[2].innerHTML != "" &&
-    $boardList[2].innerHTML == $boardList[4].innerHTML &&
-    $boardList[2].innerHTML == $boardList[6].innerHTML
-  ) {
-    return currentMove;
+  let filledFields = 0
+
+  for (const condition of winConditions) {
+    const $field0 = $boardList[condition[0]]
+    const $field1 = $boardList[condition[1]]
+    const $field2 = $boardList[condition[2]]
+
+    if (
+      $field0.innerHTML != "" &&
+      $field0.innerHTML == $field1.innerHTML &&
+      $field1.innerHTML == $field2.innerHTML
+    ) {
+      return currentMove
+    }   
   }
-  if (
-    $boardList[0].textContent != "" &&
-    $boardList[1].textContent != "" &&
-    $boardList[2].textContent != "" &&
-    $boardList[3].textContent != "" &&
-    $boardList[4].textContent != "" &&
-    $boardList[5].textContent != "" &&
-    $boardList[6].textContent != "" &&
-    $boardList[7].textContent != "" &&
-    $boardList[8].textContent != ""
-  ) {
-      return 'draw'
+
+  for (const $field of $boardList){
+    if($field.innerHTML != 0) filledFields++
+  }
+
+  if (filledFields == 9) return 'draw'
+
+}
+
+function resetBattlefield() {
+  for (const $boardItem of $boardList) {
+    $boardItem.innerHTML = ""
   }
 }
 
 function move(boardIndex) {
-  const $boardItem = $boardList[boardIndex];
+  const $boardItem = $boardList[boardIndex]
 
   if ($boardItem.innerHTML != "") {
-    return;
+    return
   }
-  $boardItem.innerHTML = currentMove;
-  const gameResult = verifyWinner();
+  $boardItem.innerHTML = currentMove
+  const gameResult = verifyWinner()
   if (gameResult == "X" || gameResult == "O") {
-    alert(currentMove);
+    // alert(currentMove)
+    addPoint(gameResult)
+    printScore()
+    setTimeout(resetBattlefield, 1000)
   }
   if (gameResult == "draw") {
-    alert("Empate");
+    // alert("Empate")
+    setTimeout(resetBattlefield, 1000)
   }
-  toggleMove();
+  toggleMove()
+}
+
+function addPoint(winner) {
+  if (winner == "X") scorePlayer1++
+  if (winner == "O") scorePlayer2++
+}
+
+function printScore() {
+  $score1.innerHTML = scorePlayer1 < 10 ? "0" + scorePlayer1 : scorePlayer1
+  $score2.innerHTML = scorePlayer2 < 10 ? "0" + scorePlayer2 : scorePlayer2
 }
 
 function addBoardListeners() {
   for (let index = 0; index < $boardList.length; index++) {
     const $boardItem = $boardList[index].addEventListener("click", function () {
-      move(index);
-    });
+      move(index)
+    })
   }
 }
-addBoardListeners();
+addBoardListeners()

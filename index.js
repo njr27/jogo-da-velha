6128c17a241c81f3925ca6b54eddd415c0dcc876
njr27 <nivaldojunior00@gmail.com>
2022-05-26 17:36:31 -0300

    Histórico de jogadas com nome e posição do campo jogado

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const $boardList = document.querySelectorAll(".square")
 const $score1 = document.querySelector(".score-p1")
 const $score2 = document.querySelector(".score-p2")
+const $winnerName = document.querySelector(".player")
+const $playerField1 = document.querySelector(".input-player-1")
+const $playerField2 = document.querySelector(".name-player-2")
+const $historyMoveList = document.querySelector(".box-history-list")
 
 let currentMove = "X"
 let scorePlayer1 = 0
@@ -17,8 +21,28 @@ const winConditions = [
   [2, 4, 6],
 ]
 
+const dictionaryIndexBoard = ['Primeiro', 'Segundo', 'Terceiro', 'Quarto', 'Quinto', 'Sexto', 'Sétimo', 'Oitavo', 'Nono']
+
+function printMoveHistory(move, playerName, boardIndex) {
+
+
+  $historyMoveList.innerHTML += `
+  <div class="box-history">
+    <span class="player-name">${move}</span>
+    <div class="history-player">
+        <span class="history-player-name">${playerName}</span>
+        <span class="history-player-place">${dictionaryIndexBoard[boardIndex]} campo</span>
+    </div>
+  </div> 
+  `
+}
+
 function toggleMove() {
   currentMove = currentMove == "X" ? "O" : "X"
+}
+
+function printWinnerName(winnerName) {
+  $winnerName.textContent = winnerName
 }
 
 function verifyWinner() {
@@ -35,15 +59,14 @@ function verifyWinner() {
       $field1.innerHTML == $field2.innerHTML
     ) {
       return currentMove
-    }   
+    }
   }
 
-  for (const $field of $boardList){
-    if($field.innerHTML != 0) filledFields++
+  for (const $field of $boardList) {
+    if ($field.innerHTML != 0) filledFields++
   }
 
-  if (filledFields == 9) return 'draw'
-
+  if (filledFields == 9) return "draw"
 }
 
 function resetBattlefield() {
@@ -60,17 +83,20 @@ function move(boardIndex) {
   }
   $boardItem.innerHTML = currentMove
   const gameResult = verifyWinner()
-  if (gameResult == "X" || gameResult == "O") {
-    // alert(currentMove)
+
+  const playerName = currentMove === "X" ? $playerField1.value : $playerField2.value
+  
+  if (gameResult === "X" || gameResult === "O") {
     addPoint(gameResult)
     printScore()
+    printWinnerName(playerName)
     setTimeout(resetBattlefield, 1000)
   }
   if (gameResult == "draw") {
-    // alert("Empate")
     setTimeout(resetBattlefield, 1000)
   }
   toggleMove()
+  printMoveHistory(currentMove, playerName, boardIndex)
 }
 
 function addPoint(winner) {
